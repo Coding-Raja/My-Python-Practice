560bef9307b1ff65b93f8743b965d38196d4ced2
Coding-Raja <rajamuhammadali628@gmail.com>
2025-09-25 23:21:03 +0500

    Using Escape Sequence of Python in my code

--- work/practice.py
+++ work/practice.py
@@ -1,14 +1,14 @@
-"""
-Write a Python program to take two numbers
-as input and display the sum of the numbers
-"""
+# Write a Python program to take two numbers as input and display the sum of the numbers
 
-n1 = int(input("Enter first number : "))
-n2 = int(input("Enter second number : "))
 
-print("The sum of numbers is : ", n1+n2)
+n1 = int(input("\nEnter first number : "))
+n2 = int(input("\nEnter second number : "))
 
-"""
-This is how you can take numbers as  input and 
-print the sum of  numbers
-"""
+print("\nThe sum of numbers is : ", n1+n2)
+
+
+# This is how you can take numbers as  input and print the sum of  numbers
+
+
+
+

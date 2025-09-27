54425ee08a44990abe3897199042484cc2724b43
Coding-Raja <rajamuhammadali628@gmail.com>
2025-09-27 23:11:24 +0500

    Study Python's for loop syntax

--- work/practice.py
+++ work/practice.py
@@ -1,17 +1,8 @@
 #-----------------------------------------------------------------------------
 
-# ----------- Match Statement in Python -----------
+# ----------- For Loop in Python -----------
 
-x = int(input("\nEnter a number : "))
+x = int(input("\nEnter a number to run the loop : "))
 
-match x:
-    case 0 : 
-        print("\nIt is 0")
-    case 1 : 
-        print("\nIt is 1")
-    case 2 : 
-        print("\nIt is 2")
-    case 3 : 
-        print("\nIt is 3")
-    case _ : 
-        print("\nIt is greater than 3")            
+for i in range(x):
+    print(i)

7ec07187547d31253b55cbeacec7ac7a2efde853
Coding-Raja <rajamuhammadali628@gmail.com>
2025-10-01 22:24:49 +0500

    For loop in lists

--- work/practice.py
+++ work/practice.py
@@ -6,3 +6,9 @@ x = int(input("\nEnter a number to run the loop : "))
 
 for i in range(x):
     print(i)
+
+#----------- For loop in lists-------------
+
+animal = ["lion", "tiger", "horse", "cat", "dog"]
+for x in animal:
+    print(x)     

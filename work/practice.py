534c2e8d03b59b2faa5e63e640fdab0e93b61f1a
Coding-Raja <rajamuhammadali628@gmail.com>
2025-10-03 09:06:08 +0500

    While loop for accessing all elements of list

--- work/practice.py
+++ work/practice.py
@@ -4,3 +4,13 @@ i = 1
 while i < 6:
   print(i)
   i += 1
+
+#----------- While loop for pring elemenst of list ------------
+
+my_list = [10, 20, 30, 40, 50]   # Define a list
+
+index = 0  # Initialize counter
+
+while index < len(my_list):   # Use while loop to iterate through the list
+    print(my_list[index])
+    index += 1

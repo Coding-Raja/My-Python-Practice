3c193754955e2f3269c40b59b54219b99979f53e
Coding-Raja <rajamuhammadali628@gmail.com>
2025-10-03 09:25:04 +0500

    Functions in Python

--- work/practice.py
+++ work/practice.py
@@ -1,16 +1,7 @@
-#----------- While Loop in Python ------------
+#-----------------------------------------------------------------------
+#------------------ Functions in Python --------------------------------
 
-i = 1
-while i < 6:
-  print(i)
-  i += 1
+def hello():
+    print("\nHello From a Function")
 
-#----------- While loop for pring elemenst of list ------------
-
-my_list = [10, 20, 30, 40, 50]   # Define a list
-
-index = 0  # Initialize counter
-
-while index < len(my_list):   # Use while loop to iterate through the list
-    print(my_list[index])
-    index += 1
+hello()    

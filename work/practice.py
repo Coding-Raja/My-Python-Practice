554ea4951c3d696dbfaf0c5300debac44cdedcf7
Coding-Raja <rajamuhammadali628@gmail.com>
2025-10-07 22:34:57 +0500

    Decorators in Python

--- work/practice.py
+++ work/practice.py
@@ -1,7 +1,25 @@
 #-----------------------------------------------------------------------
-#------------------ Functions in Python --------------------------------
+#------------------ Decorators in Python --------------------------------
 
-def hello():
-    print("\nHello From a Function")
+def changecase(func):           # This is the DECORATOR function
+  def myinner():               # This is the WRAPPER function
+    return func().upper()      # Calls original function and converts to uppercase
+  return myinner               # Returns the wrapper function
 
-hello()    
+@changecase                    # This is DECORATOR SYNTAX
+def myfunction():              # This is the ORIGINAL function
+  return "\nHello Sally"
+
+print(myfunction())
+
+
+"""
+What happens when you call myfunction():
+
+
+The decorator has replaced myfunction with myinner
+
+When you call myfunction(), you're actually calling myinner()
+
+myinner() calls the original myfunction() and applies .upper()
+"""

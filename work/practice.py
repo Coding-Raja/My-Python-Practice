ac815d5004297e57fc39c3f12b208f9b28ff5d46
Coding-Raja <rajamuhammadali628@gmail.com>
2025-10-17 22:36:54 +0500

    **kwargs in Python

--- work/practice.py
+++ work/practice.py
@@ -19,3 +19,26 @@ print(my_function(1, 2, 3))
 print(my_function(10, 20, 30, 40))
 print(my_function(5))
 """
+
+#-------------- **kwargs -----------
+"""
+If you do not know how many keyword arguments will be passed into your function, 
+add two asterisks ** before the parameter name.
+**kwargs Example 1:
+
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes")
+"""
+"""
+Example 2:
+
+def my_function(**myvar):
+  print("Type:", type(myvar))
+  print("Name:", myvar["name"])
+  print("Age:", myvar["age"])
+  print("All data:", myvar)
+
+my_function(name = "Tobias", age = 30, city = "Bergen")
+"""

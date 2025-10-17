1e8ad06df898487a35344c22c58fb35b2f6df928
Coding-Raja <rajamuhammadali628@gmail.com>
2025-10-17 22:29:28 +0500

    *args in Python

--- work/practice.py
+++ work/practice.py
@@ -1,12 +1,21 @@
 #-----------------------------------------------------------------------
-#------------------ Decorators in Python --------------------------------
-def changecase(func):
-  def myinner(x):
-    return func(x).upper()
-  return myinner
+#------------------ Python *args and **kwargs --------------------------------
 
-@changecase
-def myfunction(nam):
-  return "\nHello " + nam
+""" *args program 1
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
 
-print(myfunction("Raja"))
+my_function("Emil", "Tobias", "Linus")
+"""
+
+""" *args program 2
+def my_function(*numbers):
+  total = 0
+  for num in numbers:
+    total += num
+  return total
+
+print(my_function(1, 2, 3))
+print(my_function(10, 20, 30, 40))
+print(my_function(5))
+"""

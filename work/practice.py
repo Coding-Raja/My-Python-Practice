e143c809afc4c9dd1c25d15c40bc9e4508fb8032
Coding-Raja <rajamuhammadali628@gmail.com>
2025-10-21 22:48:30 +0500

    Python Dates

--- work/practice.py
+++ work/practice.py
@@ -1,5 +1,4 @@
-cars = ["Ford", "Volvo", "BMW"]
-x = len(cars)        #len() stores the length of array/list in x
-print(f"\n",x)     
-for x in cars:
-  print(x)
+import datetime
+
+x = datetime.datetime.now()
+print(x)
